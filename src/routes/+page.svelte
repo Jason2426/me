84395f7981848758dff6f54f3f2278337f5803b6
Jason <jay.jpy.wong@gmail.com>
2024-10-07 22:37:33 +0200

    Base v1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,29 @@
 <script>
-
+    
 </script>
 
-<div class="loading">
-    <span></span>
-    <span></span>
-    <span></span>
-  </div>
+
+<main>
+  <section class="intro-section">
+    <h1>School Projects</h1>
+    <p>
+      Welcome to my page about me, you can have a look @ the projects i've made and besides that it is a sort of play/testground for applying new techniques i've learned by myself throughout this education. 
+    </p>
+  </section>
+</main>
+
+
+
+<style>
+
+  .intro-section {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        gap: .75em;
+        margin: 20% 10% 10% 10%;
+        height: 50vh;
+  }
+</style>
