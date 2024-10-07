05f5a34deb7aeb5b51026578326ceb0a3d1e850e
Jason <jay.jpy.wong@gmail.com>
2024-10-07 22:52:25 +0200

    Feature : Map section styling.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
   </section>
   <section class="map-section">
     <button>
-      <svg width="95" height="69" viewBox="0 0 95 69" fill="none">
+      <svg width="115" height="89" viewBox="0 0 95 69" fill="none">
         <path d="M36 6H90C92.7614 6 95 8.23858 95 11V18H36V6Z" fill="#373535"/>
         <path d="M0 5C0 2.23858 2.23858 0 5 0H25C36.0457 0 45 8.95431 45 20V20H0V5Z" fill="#373535"/>
         <rect y="11.6538" width="95" height="57.3462" rx="5" fill="#545252"/>
@@ -25,13 +25,34 @@
 <style>
 
   .intro-section {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        text-align: center;
-        gap: .75em;
-        margin: 20% 10% 10% 10%;
-        height: 50vh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+      gap: .8em;
+      margin: 30% 10% 20% 10%;    
+  }
+
+  .map-section{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  button{
+    background: inherit;
+    border: none;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1em;
+  }
+
+  span {
+    font-size: 1em;
+    font-weight: 600;
+    color: #FFFFFF;
   }
 </style>
