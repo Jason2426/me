f4a25ca34f2bce1cce55c9aaf41b7b2247aa146d
Jason <jay.jpy.wong@gmail.com>
2024-10-07 22:43:52 +0200

    Feature : Map section html written.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,16 @@
       Welcome to my page about me, you can have a look @ the projects i've made and besides that it is a sort of play/testground for applying new techniques i've learned by myself throughout this education. 
     </p>
   </section>
+  <section class="map-section">
+    <button>
+      <svg width="95" height="69" viewBox="0 0 95 69" fill="none">
+        <path d="M36 6H90C92.7614 6 95 8.23858 95 11V18H36V6Z" fill="#373535"/>
+        <path d="M0 5C0 2.23858 2.23858 0 5 0H25C36.0457 0 45 8.95431 45 20V20H0V5Z" fill="#373535"/>
+        <rect y="11.6538" width="95" height="57.3462" rx="5" fill="#545252"/>
+      </svg>
+      <span>Projects</span>
+    </button>
+  </section>
 </main>
 
 <style>
