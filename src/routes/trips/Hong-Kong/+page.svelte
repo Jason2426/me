e414847cb2d9e50465c130e2050f6e1b6e8a25bb
Jason <jay.jpy.wong@gmail.com>
2024-10-10 16:43:34 +0200

    ?

--- src/routes/trips/Hong-Kong/+page.svelte
+++ src/routes/trips/Hong-Kong/+page.svelte
@@ -6,5 +6,17 @@
 
 <section class="trip-info">
     <h1>Hong Kong</h1>
+    <div class="date-of-trip">
+        <span>16/11</span>
+        <span>01/12</span>
+    </div>
+</section>
+
+<section class="img-container">
+
+</section>
+
+<section class="country-description">
+
 </section>
 
--- src/routes/trips/Singapore/+page.svelte
+++ src/routes/trips/Singapore/+page.svelte
@@ -6,4 +6,16 @@
 
 <section class="trip-info">
     <h1>Singapore</h1>
+    <div class="date-of-trip">
+        <span>19/11</span>
+        <span>22/11</span>
+    </div>
+</section>
+
+<section class="img-container">
+
+</section>
+
+<section class="country-description">
+
 </section>
