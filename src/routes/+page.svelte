d2392ca60b61f90bcf117898d85cfe77c6496995
Jason <jay.jpy.wong@gmail.com>
2024-10-10 17:27:24 +0200

    Feature : HTML written and began styling.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,61 @@
 <script>
+
 </script>
 
 
 <main>
-    <h1>Hey</h1>
+    <section class="intro-section">
+        <header>
+            <h1>
+                I LOVE
+            </h1>
+            <span> WEB? </span>
+        </header>
+        <!-- <div class="meta-info">
+            <h2>
+                @Jason <strong> Wong </strong>
+            </h2>
+
+            <h2 hidden>
+                <data value="time">hey</data>
+            </h2>
+        </div>
+        <div class="images-of-me">
+            <div class="pic-1">
+                <img src="" alt="">
+            </div>
+            <div class="pic-2">
+                <img src="" alt="">
+            </div>
+        </div>
+        <hr>
+        <div class="description">
+            <p>
+                Currently diving into front-end development to broaden my skills. I’m passionate about design, inspired by creative developers, aspiring to become one myself one day.
+            </p>
+        </div> -->
+    </section>
 </main>
 
 <style>
+    .intro-section {
+        display: flex;
+        flex-direction: column;
+    }
+
+    header {
+        display: flex;
+        gap: 1em;
+    }
+
+    h1, span {
+        display: flex;
+        text-wrap: nowrap;
+        font-size: 15vw;
+    }
+
+    /* strong {
+        color: #FFC300;
+    } */
  
 </style>
