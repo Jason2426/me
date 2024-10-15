6b10e0c896bbb9b8d192529ee609f1d212f3003d
Jason <jay.jpy.wong@gmail.com>
2024-10-15 23:55:46 +0200

    Feature : Time function written and rest of the HTML aswell

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,23 @@
 <script>
+    import { onMount } from 'svelte';
 
+    let time = '';
+    const updateTime = () => {
+        const now = new Date();
+        time = now.toLocaleTimeString(undefined, {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false
+        });
+    };
+
+    onMount(() => {
+        updateTime();
+        const interval = setInterval(updateTime, 1000);
+        return () => clearInterval(interval);
+    });
 </script>
-
 
 <main>
     <section class="intro-section">
@@ -13,14 +29,14 @@
                 WEB?
             </span>
         </header>
-        <!-- <div class="meta-info">
-            <h2>
-                @Jason <strong> Wong </strong>
-            </h2>
+        <div class="meta-info">
+            <h4>
+                <span>@Jason <strong> Wong </strong></span>
+            </h4>
 
-            <h2 hidden>
-                <data value="time">hey</data>
-            </h2>
+            <h4>
+                <span><data id="clock" value="time">Local Time : {time}</data></span>
+            </h4>
         </div>
         <div class="images-of-me">
             <div class="pic-1">
@@ -35,7 +51,21 @@
             <p>
                 Currently diving into front-end development to broaden my skills. I’m passionate about design, inspired by creative developers, aspiring to become one myself one day.
             </p>
-        </div> -->
+        </div>
+    </section>
+
+    <section class="my-work">
+        <h2>Have a look at my work</h2>
+        <p>Still working on amazing projects, that will be added to this website a.s.a.p. Scroll down to see my holiday trip collection just because I love to travel :)</p>
+        <hr>
+    </section>
+
+    <section class="my-trips">
+        <!--Foreach through an array for title and img url-->
+        <h3>Hong Kong</h3>
+        <p>Photo collection</p>
+        <hr>
+        <img src="" alt="">
     </section>
 </main>
 
@@ -51,11 +81,16 @@
         gap: 1em;
     }
 
-    span {
+    header span {
         display: flex;
         text-wrap: nowrap;
-        font-size: 15vw;
+        font-size: 14.75vw;
     }
+
+    h4 data {
+        display: none;
+    }
+
 
     span.bengals {
         /* de tekst zelf is doorzichtig */
@@ -75,25 +110,6 @@
     }
     span.bengals2 {
         transform: rotate(0.5turn);
-    }
-        @keyframes animateTextShadow {
-        0% {
-            /* de shadows bewegen allemaal naar boven */
-            text-shadow:
-            0 6em 0 #FFFFFF,
-            0 4.5em 0 #FFFFFF,
-            0 3em 0 #FFFFFF,
-            0 1.5em 0 #FFFFFF,
-            0 0 0 #FFFFFF;
-        }
-        100% {
-            text-shadow:
-            0 0 0 #FFFFFF, /* Eindpunt */
-            0 -1.5em 0 #FFFFFF,
-            0 -3em 0 #FFFFFF,
-            0 -4.5em 0 #FFFFFF,
-            0 -6em 0 #FFFFFF;
-        }
     }
 
 </style>
