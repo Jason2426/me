a8b1b6a1c2e4edf8d1a61004929e0c6a2bea5b8b
Jason <jay.jpy.wong@gmail.com>
2024-11-11 00:56:16 +0100

    Feature : Implement component on page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 
     import IntroSection from "$lib/IntroSection.svelte";
+    import MyWork from "$lib/myWork.svelte";
 
 </script>
 
@@ -8,6 +9,7 @@
 
     <IntroSection />
 
+    <MyWork/>
     
 
     <!-- <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/6m9qPYXmhge2QhBLfFKnVF?utm_source=generator&theme=0" width="50%" height="102" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe> -->
